ab3e80d9b12bdc03117c2adb30dfe25c6f8159c8
Emmanuel Agubata <emmanuel@Ogos-MacBook-Pro.local>
2019-06-20 17:04:29 -0700

    This is my commit

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-// write your code here
+$.getJSON("data.json", function(data){//this pulls all the information from the data.Json file 
+var array = []//it stores the data
+$.each(data, function(_key, val ){//it iterates through the value and grabs the information from there
+    array.push("<tr> <td>" + val.name + "</td><td>" + val.description + "</td><td><a href='https://www.google.com/maps?q="  + val.location + "'></a></td></tr>");//it pushes the data from the array into the DOM and sorts it in its positions
+    console.log(array);
+})
+$('table').append(array);//taking the informtaion from the table and appending it to the array
 
-});
+}
+
+)
+})
+
